fd40a41808837bbfbac6f1cfdc979f60db0f7dc4
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-09-27 18:30:16 -0400

    fixed import

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import SingleSkill from './singleSkill'
+import SingleSkill from './SingleSkill'
 import { Skill as SkillType } from '../typings';
 
 type Props = {

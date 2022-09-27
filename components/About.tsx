99af9004b2fb9d0a981f61f880b1d1dc2d2a139f
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-09-27 18:01:38 -0400

    redeploying

--- components/About.tsx
+++ components/About.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import Image from 'next/image'
-import myImg from '../utils/myProfile.jpg'
 import { PageInfo } from '../typings';
 import { urlFor } from '../sanity';
 

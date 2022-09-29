d6f30b9c0eef70e99a74370ebd4c6c2331ba5d95
Ahmed Sajjad <uasajjad11198@outlook.com>
2022-09-29 04:44:40 -0400

    attempting to fix spacing

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -24,7 +24,7 @@ const Projects = ({ projects }: Props) => {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5 }}
-            className='md:h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'
+            className='md:h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full  mx-auto items-center z-0'
         >
             <h3 className='md:absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl mt-20 md:mt-0'>
                 Projects
